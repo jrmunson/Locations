82111911be47cf3751442ce7fd81e91661ca2c59
Jim <jimmunson01@gmail.com>
2015-09-28 09:31:18 -0500

    Initial Commit

---
+++ Locations/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Locations
+//
+//  Created by Jim on 9/28/15.
+//  Copyright (c) 2015 Jim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
